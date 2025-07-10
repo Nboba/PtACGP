c6ea020b83a7f37d6fb9857a69d6147c1b59dca1
Nboba <nicolas.bobadilla.s@mail.pucv.cl>
2025-07-10 12:08:00 -0400

    Se agregaron comentarios

--- src/Cola.java
+++ src/Cola.java
@@ -4,22 +4,27 @@ import java.util.Optional;
 public class Cola {
     private int[] values = new int[0];
     public Cola(){}
+    public Cola(int... value){
+        for(int n : value){
+            encolar(n);
+        }
+    }
 
-    // Push
+    // Push, funcionar para agregar valores a la cola
     public void encolar(int value){
         int[] auxValues;
         auxValues = Arrays.copyOf(values,values.length + 1 );
         auxValues[values.length] = value;
         values = auxValues;
     }
-    //Pop
+    //Pop, funcion para obtener y remover el primer elemento de la cola
     public int desEncolar(){
         int[] auxValues = Arrays.copyOfRange(values,1,values.length);
         int salida = values[0];
         values = auxValues;
         return salida;
     }
-
+   // Retorna un tipo Opcional con el valor mas pequeño guardado en la cola o uno vacio.
     public Optional<Integer> minimo(){
         if(values.length == 0) return Optional.empty();
         int minimoSalida = values[0];
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args){
-        Cola cola = new Cola();
         int[] entrada=new int[]{1,23,4,5};
-
-        for(int n : entrada){
-            cola.encolar(n);
-        }
-        System.out.println(cola.minimo().get());
-        System.out.println(cola.desEncolar());
-        System.out.println(cola.minimo().get());
+        Cola cola = new Cola(entrada);
+        cola.encolar(3);
+        System.out.println("Valor minimo: "+cola.minimo().get());
+        System.out.println("Primer valor: "+cola.desEncolar());
+        System.out.println("Valor minimo: "+cola.minimo().get());
     }
 }
 
