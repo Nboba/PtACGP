76f255e92bdda18e1fc8a03d6622ac9471c4dab3
Nboba <nicolas.bobadilla.s@mail.pucv.cl>
2025-07-10 11:14:41 -0400

    Pregunta 2 Estructuras de datos

---
+++ src/Cola.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Optional;
+
+public class Cola {
+    private int[] values = new int[0];
+    public Cola(){}
+
+    // Push
+    public void encolar(int value){
+        int[] auxValues;
+        auxValues = Arrays.copyOf(values,values.length + 1 );
+        auxValues[values.length] = value;
+        values = auxValues;
+    }
+    //Pop
+    public int desEncolar(){
+        int[] auxValues = Arrays.copyOfRange(values,1,values.length);
+        int salida = values[0];
+        values = auxValues;
+        return salida;
+    }
+
+    public Optional<Integer> minimo(){
+        if(values.length == 0) return Optional.empty();
+        int minimoSalida = values[0];
+        for(int n : values){
+            if(minimoSalida > n) minimoSalida = n;
+        }
+        return Optional.of(minimoSalida);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,15 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args){
-        int[] entrada = new int[]{4, 2, 7, 2, 4, 9, 1};
-        int[] salida = ordenaryLimpiarRepetidos(entrada);
-        for(int n : salida){
-            System.out.print(n+" ");
-        }
-    }
+        Cola cola = new Cola();
+        int[] entrada=new int[]{1,23,4,5};
 
-    public static int[] ordenaryLimpiarRepetidos(int[] entrada){
-        Set<Integer> numerosUnicos = new HashSet<>();
-        for(int n: entrada){
-            numerosUnicos.add(n);
+        for(int n : entrada){
+            cola.encolar(n);
         }
-        int nUnicos = numerosUnicos.toArray().length;
-        int[] numeros = new int[nUnicos];
-        int z =0;
-        for(Integer n: numerosUnicos.toArray(new Integer[nUnicos])){
-            numeros[z++] = n;
-        }
-        for(int i =0 ; i < nUnicos; i++ ){
-            for(int j =0; j< nUnicos -1 ; j++){
-                if (numeros[j] > numeros[j +1]) {
-                    int auxNumero = numeros[j];
-                    numeros[j] = numeros[j +1];
-                    numeros[j +1] = auxNumero;                }
-            }
-        }
-        return numeros;
+        System.out.println(cola.minimo().get());
+        System.out.println(cola.desEncolar());
+        System.out.println(cola.minimo().get());
     }
 }
 
 
-//Problema:
-//Escribe una función que reciba una lista de números enteros y devuelva una nueva lista con los números ordenados, eliminando los duplicados.
-//
-//        Ejemplo:
-//entrada = [4, 2, 7, 2, 4, 9, 1]
-//salida = [1, 2, 4, 7, 9]
